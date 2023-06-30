8ae97231c17dce839a3bdcba63281cefc9fa9fba
Chaupi Amari Ghambi <amarighambi@gmail.com>
2023-06-30 11:20:16 +0200

    Port changes
    changed port to process.env

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
-const { MongoClient } = require('mongodb');
+//const { MongoClient } = require('mongodb');
 // Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
+//const url = 'mongodb://localhost:27017';
+//const client = new MongoClient(url);
 
 /*const Datastore = require('nedb');
 const database = new Datastore("database.db");*/
 
-const PORT = 7707;
-
+const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.json());
 
 app.post("/backend/api", async (req, res)=>{
-   
+	res.json({message:"Hello Chaupi"}) ;
 });
 
 app.listen(PORT, ()=>{
